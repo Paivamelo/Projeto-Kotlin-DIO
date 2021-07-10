f9acfedddb5476c1fb00b72dc35357153048a94c
Potedesorvete <65935078+Potedesorvete@users.noreply.github.com>
2021-07-10 17:28:32 -0300

    enum cliente and test to it

---
+++ Projeto-Kotlin-DIO/src/main/kotlin/one.digitalinovation.digionebank/ClienteTipo.kt
@@ -0,0 +1,17 @@
+package one.digitalinovation.digionebank
+
+import Java.math.BigDecimal
+
+class Conta(
+    val agencia: String
+    val numeroconta: String
+    val saldo: BigDecimal
+){
+    fun deposito(valor: BigDecimal){
+
+    }
+
+    fun saque(valor: BigDecimal){
+
+    }
+}
