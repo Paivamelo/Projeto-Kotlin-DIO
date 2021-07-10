15e3ab1a6f85f84e9bceb8aa3ca7082d6a946a9a
Potedesorvete <65935078+Potedesorvete@users.noreply.github.com>
2021-07-10 18:33:17 -0300

    abstract class

--- Projeto-Kotlin-DIO/src/main/kotlin/one.digitalinovation.digionebank/Pessoa.kt
+++ Projeto-Kotlin-DIO/src/main/kotlin/one.digitalinovation.digionebank/Pessoa.kt
@@ -1,23 +1,6 @@
 package one.digitalinovation.digionebank
 
-class Pessoa {
-    var nome = "Victor"
-    var cpf = "489.183.707-17"
-    private  set
-
-    constructor()
-
-    fun unirNomeCpf() = "$nome e $cpf"
-
-    //inner class Endereco{
-        //var rua: String = "Rua teste"
-  //  }
-}
-
-fun main(){
-    val victor = Pessoa()
-
-    println(victor.unirNomeCpf())
-
-    //println(victor.Endereco)
+abstract class Pessoa {
+     val nome = String,
+     val cpf = String
 }
